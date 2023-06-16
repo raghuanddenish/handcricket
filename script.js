2168ab76493f23aedcaa8bbc1a3063b33afe6362
raghuanddenish <136799340+raghuanddenish@users.noreply.github.com>
2023-06-16 18:28:37 +0530

    script.js

--- script.js
+++ script.js
@@ -2,31 +2,31 @@ var userScore = 0;
 var computerScore = 0;
 var userTurn = true;
 var gameOver = false;
+var prevChoice = null;
 
 function play(userChoice) {
   if (gameOver) return;
 
   var computerChoice = Math.floor(Math.random() * 6) + 1;
-  var total = userChoice + computerChoice;
 
   if (userTurn) {
+    if (prevChoice !== null && userChoice === prevChoice) {
+      gameOver = true;
+      document.getElementById("message").textContent = "You chose " + userChoice + ".";
+      document.getElementById("message").textContent += " You're out!";
+      document.getElementById("message").textContent += " Game over. Computer wins!";
+      return;
+    }
+
     userScore += userChoice;
     document.getElementById("message").textContent = "You chose " + userChoice + ".";
     document.getElementById("message").textContent += " Computer chose " + computerChoice + ".";
     document.getElementById("message").textContent += " Your score: " + userScore + ".";
   } else {
-    computerScore += computerChoice;
-    document.getElementById("message").textContent = "You chose " + userChoice + ".";
-    document.getElementById("message").textContent += " Computer chose " + computerChoice + ".";
-    document.getElementById("message").textContent += " Computer score: " + computerScore + ".";
-  }
-
-  if (userChoice === computerChoice) {
-    gameOver = true;
-    if (userTurn) {
-      document.getElementById("message").textContent += " You're out!";
-      document.getElementById("message").textContent += " Game over. Computer wins!";
-    } else {
+    if (prevChoice !== null && computerChoice === prevChoice) {
+      gameOver = true;
+      document.getElementById("message").textContent = "You chose " + userChoice + ".";
+      document.getElementById("message").textContent += " Computer chose " + computerChoice + ".";
       document.getElementById("message").textContent += " Computer is out!";
       if (userScore > computerScore) {
         document.getElementById("message").textContent += " Game over. You win!";
@@ -35,14 +35,16 @@ function play(userChoice) {
       } else {
         document.getElementById("message").textContent += " Game over. It's a tie!";
       }
+      return;
     }
-  } else if (userTurn && total > computerScore) {
-    gameOver = true;
-    document.getElementById("message").textContent += " You win!";
-  } else if (!userTurn && total > userScore) {
-    gameOver = true;
-    document.getElementById("message").textContent += " Computer wins!";
+
+    computerScore += computerChoice;
+    document.getElementById("message").textContent = "You chose " + userChoice + ".";
+    document.getElementById("message").textContent += " Computer chose " + computerChoice + ".";
+    document.getElementById("message").textContent += " Computer score: " + computerScore + ".";
   }
+
+  prevChoice = userTurn ? userChoice : computerChoice;
 
   userTurn = !userTurn;
 }
